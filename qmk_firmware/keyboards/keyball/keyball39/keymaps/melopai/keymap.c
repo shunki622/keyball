03e6e74d6528de5f09405a4e05383f6a0c3502c0
melopai <o8084237368@gmail.com>
2026-05-28 13:49:26 +0900

    コンボtab コンボ秒数200

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/melopai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/melopai/keymap.c
@@ -71,9 +71,9 @@ void oledkit_render_info_user(void) {
 #endif
 
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM my_jq[] = {KC_J, KC_Q, COMBO_END};
+const uint16_t PROGMEM my_tab[] = {KC_Q, KC_W, COMBO_END};
 
 combo_t key_combos[] = {
-COMBO(my_jq, KC_QUES),
+COMBO(my_tab, KC_TAB),
 };
 #endif
